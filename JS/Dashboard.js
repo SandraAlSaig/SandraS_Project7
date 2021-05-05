b4d3fe5a16c102f7ce6bcba5dfac5cc11543d636
SandraS <alsaigsandra@gmail.com>
2021-05-05 16:51:08 +0200

    Work in progress ;)

---
+++ JS/Dashboard.js
@@ -0,0 +1,11 @@
+let bellSvg = getElementById('#bellsvg');
+let notification = getElementById('notification-dropdown');
+let alertBanner = getElementById('alert');
+const trafficCanvas = document.querySelector('#traffic-chart');
+const dailyCanvas = document.querySelector('#daily-chart');
+const doughnutCanvas = document.querySelector('#doughnut-chart');
+
+
+bellSvg.addEventListener (click, () => {
+  alert("It works!");
+});
