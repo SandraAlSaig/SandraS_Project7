526c89c5a23863d9c435e7161bb5a1f42104ec01
SandraS <alsaigsandra@gmail.com>
2021-05-06 16:44:42 +0200

    trying to add dropdown menu to SVG

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -1,11 +1,15 @@
-let bellSvg = getElementById('#bellsvg');
-let notification = getElementById('notification-dropdown');
-let alertBanner = getElementById('alert');
+let bellSvg = document.getElementById('bellsvg');
+let notification = document.getElementById('dropBtn');
+let alertBanner = document.getElementById('alert');
 const trafficCanvas = document.querySelector('#traffic-chart');
 const dailyCanvas = document.querySelector('#daily-chart');
 const doughnutCanvas = document.querySelector('#doughnut-chart');
 
 
-bellSvg.addEventListener (click, () => {
-  alert("It works!");
-});
+// bellSvg.addEventListener ("click", () => {
+//   alert("It works!");
+// });
+//
+// notification.addEventListener("click", () => {
+//   alert("It works!");
+// })
