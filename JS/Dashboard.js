47cf5edeb40b1e3446cc6ce25952fdbdf05ffcf2
SandraS <alsaigsandra@gmail.com>
2021-05-12 10:39:23 +0200

    updated canvas..

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -2,7 +2,6 @@ let bellSvg = document.getElementById('bellsvg');
 let trafficNav = document.querySelectorAll('.traffic-nav-link');
 let alertBanner = document.getElementById('alert');
 const trafficCanvas = document.querySelector('#traffic-chart');
-const dailyCanvas = document.querySelector('#daily-chart');
 const doughnutCanvas = document.querySelector('#doughnut-chart');
 
 
