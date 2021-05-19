9bb4e2f02fe41e8596556f48c0d0b9ec67a7116e
SandraS <alsaigsandra@gmail.com>
2021-05-19 19:59:14 +0200

    Style changes and adjustments

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -5,6 +5,19 @@ const trafficCanvas = document.querySelector('#traffic-chart');
 const doughnutCanvas = document.querySelector('#doughnut-chart');
 
 
-// bellsvg.addEventListener ("click", () => {
-//   alert("It works!");
-// });
+alertBanner.innerHTML =
+`
+  <div class="alert-banner">
+  <p><strong>Alert:</strong> You have <strong>3</strong> overdue tastks to complete</p>
+  </div>
+  <div>
+  <p class="alert-banner-close">x</p>
+  </div>
+`
+  alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+      alert.style.display = "none"
+
+    }
+  });
